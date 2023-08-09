58d3ff5b1671686c0c6fd6d3329f0fdad22fdbd9
Karthick <karthickbharathi15@gmail.com>
2023-08-09 18:20:46 +0530

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,13 @@ import Component42 from '@/assets/Component 42.png'
 import Component43 from '@/assets/Component 43.png'
 import Component44 from '@/assets/Component 44.png'
 
+import div from '@/assets/div.png'
+import div1 from '@/assets/div (1).png'
+
+import coach1 from '@/assets/kindpng_218182.png'
+import coach2 from '@/assets/Frame 39.png'
+import coach3 from '@/assets/Frame 40.png'
+
 
 export default function Home() {
   return (
@@ -27,7 +34,7 @@ export default function Home() {
       <div className='relative min-h-[90vh] bg-opacity-20'>
         {/* <div className="w-full bg-[#00000090]"></div> */}
         
-        <video controls={false} autoPlay loop className="w-full h-full"  >
+        <video controls={false} autoPlay loop muted className="w-full h-screen object-cover aboslute top-0 left-0"  >
           <source src="/fitness.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
@@ -43,6 +50,46 @@ export default function Home() {
           <ContactButton />
         </div>
       </div>
+
+
+      {/* ===========================Types of Programs===================== */}
+
+     
+        <div className='h-screen lg:flex justify-around px-[20px] lg:px-[100px] lg:pt-[50px]'>
+          <div className='lg:w-2/6 h-1/3 lg:h-full'>
+          <div className='h-full lg:h-[75%] overflow-y-scroll no-scrollbar mt-[100px]  md:pl-[100px] '>
+            <Image className=' w-full md:w-[90%]  my-4 hover:shadow-white hover:shadow-lg hover:scale-110 duration-300' src={div} alt='Weight Management' />
+            <Image className=' w-full md:w-[90%]  my-4 hover:shadow-white hover:shadow-lg hover:scale-110 duration-300' src={div1} alt='Weight Management' />
+            <Image className=' w-full md:w-[90%]  my-4 hover:shadow-white hover:shadow-lg hover:scale-110 duration-300' src={div} alt='Weight Management' />
+            <Image className=' w-full md:w-[90%]  my-4 hover:shadow-white hover:shadow-lg hover:scale-110 duration-300' src={div1} alt='Weight Management' />
+            <Image className=' w-full md:w-[90%]  my-4 hover:shadow-white hover:shadow-lg hover:scale-110 duration-300' src={div} alt='Weight Management' />
+
+          </div>
+
+          </div>
+         
+          <div className='lg:w-[45%]'>
+              <div className='w-full'>
+                  <div className='text-[#AFCCF8] text-center text-[24px] md:text-[40px] font-bold'>Workout Program</div>
+          
+                  <div className='text-center text-[24px] md:text-[40px] font-bold text-[#FED25B]'>made for you</div>
+              </div>
+              <div className="rounded-3xl bg-gradient-to-r from-[#4A2F70] to-[#344363] px-[25px] py-5  lg:px-[55px] lg:py-10">
+                <h1 className='text-center font-bold text-[24px] md:text-[30px] text-[#FED25B]'>Weight Management</h1>
+                <p className='text-center text-white text-xs sm:text-lg'>
+                Gorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum
+                </p>
+                <div className='flex justify-center mt-5'>
+                <button className=' rounded-lg py-2  px-20 bg-[#aeb5e0]'>Join Now</button>
+
+                </div>
+               
+              </div>
+              
+
+          </div>
+        </div>
+   
 
       {/* <div className="mt-8">
         
@@ -97,7 +144,129 @@ export default function Home() {
         </div>
       </div>
 
+
+
+      {/* ==============================YOGA COACHES======================= */}
+      <div  className='h-screen sm:px-[100px] sm:py-[60px] px-[40px] py-[20px]'>
+        <div className='text-2xl sm:text-[50px]  '>
+            <p className='sm:mb-8 lg:mb-12 text-[#AFCCF8]  font-[600] text-center'>
+            Reax your body nd mind
+
+            </p>
+            <p  className = 'text-[#F2BD4D]  font-[600] text-center sm:mb-12 '>With your yoga coaches</p>
+        </div>
+       
+       <div className='overflow-x-auto  whitespace-nowrap'>
+       <div className='slide-track     space-x-[200px] w-full mt-6 '>
+            
+            <div className=' inline-block  flex-shrink-0 text-white '>
+                <Image className=' h-[431px]    object-cover   hover:scale-105 duration-300' src={coach1} alt="/" />
+                
+                <p className='text-center'  >Gurpreet Singh</p>
+                <p className='text-center' >E-REPS</p>
+
+            </div> 
+           <div className=' inline-block  flex-shrink-0  text-white  '>
+                <Image className='h-[431px]    object-cover  hover:scale-105 duration-300 mb-3' src={coach2} alt="/" />
+                
+                <p className='text-center'  >Gurpreet Singh</p>
+                <p className='text-center' >E-REPS</p>
+
+            </div>
+             
+            <div className='  inline-block  flex-shrink-0  text-white  '>
+                <Image className=' h-[431px]    object-cover  hover:scale-105 duration-300' src={coach3} alt="/" />
+                
+                <p className='text-center'  >Gurpreet Singh</p>
+                <p className='text-center' >E-REPS</p>
+
+            </div>
+            <div className=' inline-block   flex-shrink-0 text-white '>
+                <Image className=' h-[431px]    object-cover  hover:scale-105 duration-300' src={coach1}  alt="/" />
+                
+                <p className='text-center'  >Gurpreet Singh</p>
+                <p className='text-center' >E-REPS</p>
+
+            </div>
+            <div className='  inline-block  flex-shrink-0 text-white '>
+                <Image className=' h-[431px]  object-cover  hover:scale-105 duration-300' src={coach2}  alt="/" />
+                
+                <p className='text-center'  >Gurpreet Singh</p>
+                <p className='text-center' >E-REPS</p>
+
+            </div>
+            <div className='  inline-block  flex-shrink-0 text-white '>
+                <Image className=' h-[431px] object-cover  hover:scale-105 duration-300' src={coach3}  alt="/" />
+                
+                <p className='text-center'  >Gurpreet Singh</p>
+                <p className='text-center' >E-REPS</p>
+
+            </div>
+            <div className='  inline-block  flex-shrink-0 text-white '>
+                <Image className=' h-[431px]    object-cover   hover:scale-105 duration-300' src={coach1} alt="/" />
+                
+                <p className='text-center'  >Gurpreet Singh</p>
+                <p className='text-center' >E-REPS</p>
+
+            </div> 
+           <div className='  inline-block  flex-shrink-0  text-white  '>
+                <Image className='h-[431px]    object-cover  hover:scale-105 duration-300 mb-3' src={coach2} alt="/" />
+                
+                <p className='text-center'  >Gurpreet Singh</p>
+                <p className='text-center' >E-REPS</p>
+
+            </div>
+             
+            <div className='  inline-block  flex-shrink-0  text-white  '>
+                <Image className=' h-[431px]    object-cover  hover:scale-105 duration-300' src={coach3} alt="/" />
+                
+                <p className='text-center'  >Gurpreet Singh</p>
+                <p className='text-center' >E-REPS</p>
+
+            </div>
+            <div className='  inline-block   flex-shrink-0 text-white '>
+                <Image className=' h-[431px]    object-cover  hover:scale-105 duration-300' src={coach1}  alt="/" />
+                
+                <p className='text-center'  >Gurpreet Singh</p>
+                <p className='text-center' >E-REPS</p>
+
+            </div>
+            <div className=' inline-block  flex-shrink-0 text-white '>
+                <Image className=' h-[431px]  object-cover  hover:scale-105 duration-300' src={coach2}  alt="/" />
+                
+                <p className='text-center'  >Gurpreet Singh</p>
+                <p className='text-center' >E-REPS</p>
+
+            </div>
+            <div className='  inline-block  flex-shrink-0 text-white '>
+                <Image className=' h-[431px] object-cover  hover:scale-105 duration-300' src={coach3}  alt="/" />
+                
+                <p className='text-center'  >Gurpreet Singh</p>
+                <p className='text-center' >E-REPS</p>
+
+            </div>
+            
+            
+              
+
+        
+        
+    </div> 
+    </div>
+       
+        
+        
+            
+        
+    </div>
+
+
+
+
+
+
       {/* ===================== {Contact Form} ===================== */}
+
       <ContactForm />
     </>
 
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -36,17 +36,17 @@ const ContactForm = () => {
     return (
     <div className="py-5 px-4 md:px-8 lg:px-12 my-8">
         <div className="rounded-3xl bg-gradient-to-r from-[#4A2F70] to-[#344363] px-8 py-8 lg:py-14" id="contactForm">
-            <div className="flex flex-col lg:flex-row md:gap-x-10">
+            <div className="flex flex-col md:flex-row md:gap-x-10">
 
-                <div className="text-gray-100 col-span-1 w-5/12 my-auto">
+                <div className="text-gray-100 col-span-1  md:w-5/12 my-auto">
                     <h2 className="text-5xl font-semibold text-center">Stay tuned!</h2>
                     <p className="max-w-[80%] mx-auto text-center mt-5">Get the latest articles and business updates that you need to know, you’ll even get special recommendations weekly.</p>
                 </div>
 
-                <div className="col-span-1 my-auto w-4/12">
+                <div className="col-span-1 my-auto md:w-4/12">
                     <label htmlFor='emailInput' className="py-2 pl-5 pr-3 bg-gray-300 bg-opacity-30 rounded-full my-auto flex">
                         <input type="text" id="emailInput" onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" className="pr-3 bg-transparent outline-none w-8/12 border-none text-white" />
-                        <button onClick={handleSubmit} className="bg-[#FED25B] rounded-full px-4 py-2 w-4/12 text-black font-semibold">Sign Up</button>
+                        <button onClick={handleSubmit} className="bg-[#FED25B] rounded-full px-3 py-1 md:px-4 md:py-2 md:w-4/12 text-black font-semibold">Sign Up</button>
                     </label>
                 </div>
                 
