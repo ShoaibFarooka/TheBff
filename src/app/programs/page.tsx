f1fe4dfe9851ff5f3b267183b9dba0a03e7836a5
Karthick <karthickbharathi15@gmail.com>
2023-08-17 14:53:51 +0530

    update

--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 "use client";
 
 import React, { useState } from "react";
@@ -31,6 +32,9 @@ import image4 from "@/assets/No equipment.png";
 import image5 from "@/assets/Toning.png";
 import image6 from "@/assets/Walking.png";
 
+
+import cross from '@/assets/Cross.png'
+import tick from '@/assets/Vector 2.png'
 const ImageWithTitle = ({
   image,
   title,
@@ -39,6 +43,8 @@ const ImageWithTitle = ({
   title: string;
 }) => {
   return (
+
+
     <div className="col-span-1 w-full h-[12rem] md:h-[18rem] lg:h-[22rem]">
       <div className="relative mx-auto hover:shadow shadow-purple-300 w-[90%] h-full">
         <Image
@@ -85,6 +91,17 @@ export default function Page() {
   const goToSlide = (slideIndex: React.SetStateAction<number>) => {
     setCurrentIndex(slideIndex);
   };
+
+
+
+  const [overlayVisible, setOverlayVisible] = useState(false);
+
+  const toggleOverlay = () => {
+    setOverlayVisible(!overlayVisible);
+  };
+  const onClose = () =>{
+    setOverlayVisible(!overlayVisible);
+  }
 
   
   return (
@@ -162,7 +179,7 @@ export default function Page() {
       </div>
 
       {/* ============================== Pricing ======================= */}
-      <div className="my-20">
+      <div className="my-20 relative">
         <div className="max-w-2xl mx-auto">
           <h1 className="text-center font-semibold text-[40px] lg:text-[72px] text-[#F2BD4D]">
             Choose Your Plan
@@ -244,11 +261,11 @@ export default function Page() {
               </div>
               <div className="col-span-1 py-3 border border-gray-400/40 center flex-col">
                 <p className="text-base md:text-xl text-center mb-2.5"> Starting at Rs. 1999/- month </p>
-                {/* <button className="shadow-xl shadow-red-800/10 mx-auto rounded px-2 py-1.5 border-none outline-none bg-white text-red-500 font-semibold focus:border"> View Plans </button> */}
+                <button onClick={toggleOverlay} className="shadow-xl shadow-red-800/10 mx-auto rounded px-2 py-1.5 border-none outline-none bg-white text-red-500 font-semibold focus:border"> View Plans </button>
               </div>
               <div className="col-span-1 py-3 border border-gray-400/40 center flex-col">
                 <p className="text-base md:text-xl text-center mb-2.5"> Starting at Rs. 3499/- month </p>
-                {/* <button className="shadow-xl shadow-red-800/10 mx-auto rounded px-2 py-1.5 border-none outline-none bg-white text-red-500 font-semibold focus:border"> View Plans </button> */}
+                <button onClick={toggleOverlay} className="shadow-xl shadow-red-800/10 mx-auto rounded px-2 py-1.5 border-none outline-none bg-white text-red-500 font-semibold focus:border"> View Plans </button>
               </div>
 
 
@@ -258,6 +275,174 @@ export default function Page() {
           </div>
 
         </div>
+
+
+
+
+
+
+
+              {/* ==========OVERLAY============ */}
+
+        {overlayVisible && 
+        <div className="absolute top-0 left-0 w-full h-full  flex justify-center   z-50 bg-opacity-70 backdrop-blur">
+          <div className="w-11/12 px-10 py-10 bg-gradient-to-r from-[#4A2F70] to-[#344363] rounded-[24px] ">
+          <div className="w-full   mx-auto relative flex justify-center items-center ">
+          <button
+          onClick={onClose}
+          className="absolute top-2 left-2 bg-white rounded-2xl"
+        >
+          <Image src={cross}  alt=""/>
+        </button>
+            <h2 className="text-5xl font-bold text-[#F2BD4D] text-center mb-4 ">
+              
+              Choose Plan
+            </h2>
+          </div>
+          <div className="w-full   mx-auto mb-4 ">
+            <p className="text-white text-center">Gorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus.</p>
+          </div>
+          <div className="flex justify-center items-center ">
+            <div className="inline bg-white px-1 py-1 rounded-md">
+            <button className="px-4 py-2 hover:bg-blue-300 hover:text-white rounded-md"> 1 month </button>
+            <button className="px-4 py-2 hover:bg-blue-300 hover:text-white rounded-md">3 month </button>
+            <button className="px-4 py-2 hover:bg-blue-300 hover:text-white rounded-md" >6 month</button>
+
+            </div>
+            
+          </div>
+          <div className="flex justify-center items-center py-12 ">
+
+            <div className="bg-[#E7E7FF] py-10 px-10 rounded-lg mx-5">
+              <h2 className="font-[600] text-2xl text-center">Standard</h2>
+              <h3 className="text-center">1999 month</h3>
+              <hr className="border border-black mt-10"/>
+              <div className="mt-10"> 
+                <div >
+                <button className="px-2 py-2 bg-[#363535] rounded-3xl mr-2">
+                <Image src={tick} alt="" />
+                
+                </button>
+                <span>Create personal dashboard</span>
+
+                </div >
+                
+                <div className="mt-5">
+                  <button className="px-2 py-2 bg-[#363535] rounded-3xl mr-2">
+                  <Image src={tick} alt="" />
+                  
+                  </button>
+                  <span>Trainer Support</span>
+                </div>
+                
+                <div className="mt-5">
+                  <button className="px-2 py-2 bg-[#363535] rounded-3xl mr-2 ">
+                  <Image src={tick} alt="" />
+                
+                  
+                  </button>
+                  <span>Rewards & Achievement's</span>
+
+                </div>
+                <div className="flex items-center justify-center py-3">
+                <button className="px-3 py-1 rounded-lg  text-white bg-[#6557FF]">Buy Now</button>
+
+                </div>
+                
+               
+                
+              </div>
+            </div>
+
+
+
+
+            <div className="bg-[#FFCC61] py-[70px] px-10 rounded-lg mx-5">
+              <div className="text-center">
+                <button className="text-white bg-[#6557FF] px-1 rounded-md">-30%</button>
+              </div>
+              <h2 className="font-[600] text-2xl  text-center">Premium</h2>
+              <h3 className="text-center">3499 month</h3>
+              <hr className="border border-black mt-10"/>
+              <div className="mt-10"> 
+                <div >
+                <button className="px-2 py-2 bg-[#363535] rounded-3xl mr-2">
+                <Image src={tick} alt="" />
+                
+                </button>
+                <span>All features in Stadard</span>
+
+                </div >
+                
+                <div className="mt-5">
+                  <button className="px-2 py-2 bg-[#363535] rounded-3xl mr-2">
+                  <Image src={tick} alt="" />
+                  
+                  </button>
+                  <span>Pause Membership on your ease</span>
+                </div>
+                
+                <div className="mt-5">
+                  <button className="px-2 py-2 bg-[#363535] rounded-3xl mr-2">
+                  <Image src={tick} alt="" />
+                
+                  
+                  </button>
+                  <span>Custom Nutrition Plans</span>
+
+                </div>
+                <div className="flex items-center justify-center py-3">
+                <button className="px-3 py-1 rounded-lg  text-white bg-[#6557FF]">Buy Now</button>
+
+                </div>
+                
+               
+                
+              </div>
+            </div>
+            <div className="bg-[#E7E7FF] py-10 px-10 rounded-lg mx-5">
+              <h2 className="font-[600] text-2xl  text-center">Enterprise</h2>
+              <h3 className="text-center">Contact Us</h3>
+              <hr className="border border-black mt-10"/>
+              <div className="mt-10"> 
+                <div >
+                <button className="px-2 py-2 bg-[#363535] rounded-3xl mr-2">
+                <Image src={tick} alt="" />
+                
+                </button>
+                <span>All features in Premium Plan</span>
+
+                </div >
+                
+                <div className="mt-5">
+                  <button className="px-2 py-2 bg-[#363535] rounded-3xl mr-2">
+                  <Image src={tick} alt="" />
+                  
+                  </button>
+                  <span>Bulk Discount</span>
+                </div>
+                
+                <div className="mt-5">
+                  <button className="px-2 py-2 bg-[#363535] rounded-3xl mr-2">
+                  <Image src={tick} alt="" />
+                
+                  
+                  </button>
+                  <span>24*7 Support</span>
+
+                </div>
+                <div className="flex items-center justify-center py-3">
+                <button className="px-3 py-1 rounded-lg  text-white bg-[#6557FF]">Buy Now</button>
+
+                </div>
+                
+               
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        }
       </div>
 
       <ContactForm />
