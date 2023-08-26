05a7b9fbcdc388828b2e71d29439116e196b2da5
siddiquiaffan <siddiquiaffan201@gmail.com>
2023-08-26 12:48:18 +0530

    typo

--- app/api/auth/reset-password/route.ts
+++ app/api/auth/reset-password/route.ts
@@ -7,12 +7,8 @@ export const POST = async (req: NextRequest) => {
 
     if (!email) return new Response(JSON.stringify({ error: "Email is required" }), { status: 400 })
 
-    const res = await 
-    
-    (email);
+    const res = await sendResetPasswordEmail(email);
 
-    
-    console.log(res)
     if (!res.success) return new Response(JSON.stringify({ message: res.message }), { status: 400 })
 
     return new Response(JSON.stringify({ success: true, message: res.message ?? 'Password reset link sent successfully!' }), { status: 200 })
