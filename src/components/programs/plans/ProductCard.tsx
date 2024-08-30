032ca8723cfc79343faa8c3b98d793fe1a2abddb
siddiquiaffan <siddiquiaffan201@gmail.com>
2024-08-30 23:13:09 +0530

    chore: Remove unused handleSubscribe function and related code in ProductCard component

--- src/components/programs/plans/ProductCard.tsx
+++ src/components/programs/plans/ProductCard.tsx
@@ -2,20 +2,12 @@
 // import cross from "@/assets/Cross.png";
 // import { Dialog, DialogContent } from "@/components/ui/dialog";
 // import { getSubscriptions } from "@/lib/stripe";
-import { getServerData } from "@/lib/utils";
 import { Subscription } from "@/types/db";
 import { Plan } from "@/types/subscription";
 import clsx from "clsx";
 import Link from "next/link";
-import { TransitionStartFunction } from "react";
-import toast from "react-hot-toast";
 import { FaCircleCheck } from "react-icons/fa6";
 // import { RxCross2 } from "react-icons/rx";
-import { useAuth } from "@/hooks/auth";
-import { cookie } from "@/lib/dom";
-import { makeSubscriptionPayment } from "@/lib/subscription/client";
-import { createSubscription, verifyPayment } from "@/lib/subscription/server";
-import { useRouter } from "next/navigation";
 import Script from "next/script";
 
 // interface Interval {
@@ -27,99 +19,17 @@ interface ProductCardProps {
   plan: Plan;
   //   billingInterval: Interval;
   subscription?: Subscription;
-  isLoading: boolean;
-  startTransition: TransitionStartFunction;
 }
 
 const ProductCard = ({
   //   billingInterval,
   plan,
   subscription,
-  isLoading,
-  startTransition,
 }: ProductCardProps) => {
-  const { user } = useAuth();
-  const router = useRouter();
-
   const price = plan.item.amount ? plan.item.amount / 100 : "";
   const isSubscriptionActive = ["active", "created"].includes(
     subscription?.status as any as string
   );
-
-  // useDebug('isLoading: ' + isLoading)
-
-  const handleSubscribe = async () => {
-    const tid = toast.loading("Processing...");
-
-    try {
-      const { error, subscriptionId } = await getServerData<
-        ReturnType<typeof createSubscription>
-      >(startTransition, async () => {
-        // const fromCookie = cookie.get(`subscription-${plan.id}`);
-        // if (fromCookie) return { subscriptionId: fromCookie };
-
-        const res = await createSubscription({ planId: plan.id });
-        return res;
-      });
-
-      if (error || !subscriptionId)
-        return toast.error(`Failed to create subscription: \n${error}`, {
-          id: tid,
-        });
-
-      // save subscription id in cookie for 10 minutes
-      // cookie.set(`subscription-${plan.id}`, subscriptionId, 60 * 10);
-
-      toast.loading(
-        `Processing payment for subscription ${subscriptionId}...`,
-        { id: tid }
-      );
-
-      // verifyPayment, returns either error or subscription & payment details
-      await makeSubscriptionPayment(subscriptionId, {
-        user,
-        onSuccess: async (response: any) => {
-          const res = await getServerData<ReturnType<typeof verifyPayment>>(
-            startTransition,
-            async () =>
-              verifyPayment({
-                paymentId: response.razorpay_payment_id,
-                subscriptionId,
-              })
-          );
-
-          if (res.error) {
-            toast.error(`Failed to verify payment: \n${res.error}`, {
-              id: tid,
-            });
-            return;
-          }
-
-          toast.success(
-            `You have successfully subscribed to ${plan.item.name}. Redirecting to your dashboard...`,
-            { id: tid }
-          );
-
-          // delete cookie
-          cookie.delete(`subscription-${plan.id}`);
-
-          // sleep for 2 seconds
-          await new Promise((resolve) => setTimeout(resolve, 2000));
-          router.push("/dashboard");
-        },
-
-        onError: async (response: any) => {
-          toast.error(
-            `Payment failed: \n${response.error}. \nPlease try again or contact us for support.`,
-            { id: tid }
-          );
-        },
-      });
-    } catch (err: any) {
-      console.log(err);
-      toast.error(err.message ?? "Something went wrong.", { id: tid });
-    }
-  };
 
   return (
     <>
@@ -177,8 +87,8 @@ const ProductCard = ({
                 <Link href={`/checkout?plan=${plan.id}`}>
                   <button
                     className="px-5 py-2 rounded-lg text-white bg-[#6557FF] center gap-2 disabled:opacity-75 hover:opacity-90"
-                    // disabled={isLoading}
-                    // onClick={handleSubscribe}
+                  // disabled={isLoading}
+                  // onClick={handleSubscribe}
                   >
                     Subscribe
                     {/* {isLoading && <Spinner size={15} />} */}
