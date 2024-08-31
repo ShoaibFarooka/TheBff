197d05ac74a57d81ceda921414693862ac0eda35
siddiquiaffan <siddiquiaffan201@gmail.com>
2024-08-31 21:55:34 +0530

    chore: Update site URL in helpers.ts

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -6,7 +6,7 @@ import { Price } from "@/types/db";
 
 export const getURL = () => {
     let url =
-        process?.env?.NEXT_PUBLIC_APP_URL ?? // Set this to your site URL in production env.
+        process?.env?.NEXT_PUBLIC_APP_URL ?? // Set this to your app URL in production env.
         process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
         'http://localhost:3000/';
     // Make sure to include `https://` when not localhost.
