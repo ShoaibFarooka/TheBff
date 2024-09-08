7ce942af53bd96417fda34c1d4092ac5dbb585e8
siddiquiaffan <siddiquiaffan201@gmail.com>
2024-09-08 22:59:31 +0530

    Refactor root.ts to add planRouter to appRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { cartRouter } from "./routers/cart/cart.procedure";
 import { couponRouter } from "./routers/coupon/coupon.procedure";
 import { paymentRouter } from "./routers/payment/payment.procedure";
+import { planRouter } from "./routers/plan/plan.procedure";
 import { subscriptionRouter } from "./routers/subscription/subscription.procedure";
 import { userRouter } from "./routers/user/user.procedure";
 import { createTRPCRouter } from "./trpc";
@@ -11,6 +12,7 @@ export const appRouter = createTRPCRouter({
   subscriptions: subscriptionRouter,
   user: userRouter,
   coupon: couponRouter,
+  plan: planRouter,
 });
 
 export type AppRouter = typeof appRouter;
