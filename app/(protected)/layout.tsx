e54f746272d97ce90eac0e12c44c18ea9974c98e
siddiquiaffan <siddiquiaffan201@gmail.com>
2024-09-08 22:50:29 +0530

    Refactor protected layout to remove unnecessary code and improve authentication flow

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,15 +1,11 @@
 import { authenticate } from '@/lib/auth';
-import { logger } from '@/lib/logger';
 import { redirect } from "next/navigation";
 
 const Layout = async ({ children }: any) => {
 
-    
     const auth = await authenticate();
     if (!auth.success || auth.unAuthenticated)
         return redirect("/login");
-
-    logger.log('Hello from protected layout')
 
     return <>{children}</>;
 };
