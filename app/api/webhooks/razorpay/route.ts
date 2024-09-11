fd6b06a3790c22b3b71ce7685f1d51fc0432763d
siddiquiaffan <siddiquiaffan201@gmail.com>
2024-09-11 15:32:48 +0530

    Refactor webhook route to send confirmation and notification emails

--- app/api/webhooks/razorpay/route.ts
+++ app/api/webhooks/razorpay/route.ts
@@ -1,12 +1,20 @@
 // import type {  } from 'razorpay'
 import connectDB from "@/lib/dbConnection";
-import { logger } from "@/lib/logger";
+import { sendEmail } from "@/lib/email";
+import { logger, prodLogger } from "@/lib/logger";
+import { adminNotificationTemplate, subscriptionConfirmationTemplate } from "@/lib/templates/subscriptionConfirmation";
 import { Order, Subscription, User } from "@/models";
-import { SubscriptionStatus } from "@/types/subscription";
+import { Plan as PlanType, SubscriptionStatus, Subscription as SubscriptionType } from "@/types/subscription";
+import { User as UserType } from "@/types/user";
 import Razorpay from "razorpay";
 import { Orders } from "razorpay/dist/types/orders";
 import { Payments } from "razorpay/dist/types/payments";
 
+interface Meta {
+    plans: string;
+    userId: string;
+    customerId: string;
+}
 
 const relevantEvents = new Set([
     "order.paid"
@@ -37,11 +45,7 @@ export async function POST(req: Request) {
         const orderEntity = body.payload.order.entity as Orders.RazorpayOrder;
         const paymentEntity = body.payload.payment.entity as Payments.RazorpayPayment;
 
-        const meta = orderEntity.notes as {
-            plans: string;
-            userId: string;
-            customerId: string;
-        };
+        const meta = orderEntity.notes as unknown as Meta;
 
         // update subscription status
         const planIds = meta.plans.split(",").map(id => id.trim());
@@ -57,7 +61,7 @@ export async function POST(req: Request) {
         await Promise.all([
             User.updateOne(
                 { _id: meta.userId },
-                { customerId: meta.customerId }
+                { razorpayCustomerId: meta.customerId }
             ),
             Subscription.updateMany({
                 userId: meta.userId,
@@ -70,12 +74,73 @@ export async function POST(req: Request) {
                 { id: order.id }, // order id
                 order,
                 { upsert: true }
-            )
+            ),
+            sendConfirmationEmails(order, meta)
         ])
 
         return new Response("Webhook processed", { status: 200 });
     } catch (error) {
         logger.error("🔔❌ Webhook processing failed:", error);
         return new Response("Webhook processing failed", { status: 500 });
+    }
+}
+
+async function sendConfirmationEmails(
+    order: Orders.RazorpayOrder,
+    meta: Meta
+) {
+    try {
+        await connectDB();
+
+        const [subscriptions, user] = await Promise.all([
+            Subscription
+                .find({
+                    planId: { $in: meta.plans.split(",").map(id => id.trim()) },
+                    userId: meta.userId,
+                })
+                .populate<PlanType>("plan")
+                .lean<Array<SubscriptionType & { plan: PlanType }>>(),
+            User
+                .findOne({ _id: meta.userId }).lean<UserType>()
+        ])
+
+        if (!user || !subscriptions.length) {
+            return;
+        }
+
+        const formattedPlans = subscriptions.map(sub => ({
+            name: sub.plan.name,
+            amount: sub.plan.amount ?? 0,
+            startDate: (sub.startDate as Date).toLocaleDateString('en-In', { year: 'numeric', month: 'short', day: 'numeric' }), // 01 Jan 2022 
+            endDate: (sub.endDate as Date).toLocaleDateString('en-In', { year: 'numeric', month: 'short', day: 'numeric' })
+        }))
+
+        const userEmailPromise = sendEmail({
+            from: `TheBFF Subscriptions <${process.env.EMAIL_USER}>`,
+            to: user.email,
+            subject: "Your order(s) has been confirmed",
+            text: `Your order has been confirmed.`,
+            html: subscriptionConfirmationTemplate({
+                plans: formattedPlans,
+                totalAmount: order.amount as number
+            })
+        })
+
+        const adminEmailPromise = sendEmail({
+            from: `TheBFF Subscriptions <${process.env.EMAIL_USER}>`,
+            to: process.env.EMAIL_USER!,
+            subject: "New subscription order",
+            text: `New subscription order`,
+            html: adminNotificationTemplate({
+                user,
+                plans: formattedPlans,
+                totalAmount: order.amount as number
+            })
+        })
+
+        await Promise.all([userEmailPromise, adminEmailPromise]);
+
+    } catch (error) {
+        prodLogger.error("Failed to send confirmation emails", error);
     }
 }
